c3a3809f4d581e6e3a7b275a2f4c24de4508c017
g14954wy <g14954wy@m20202.tsuda.ac.jp>
2016-04-20 19:40:57 +0900

    2回目のコミットだお

--- src/com/toy/anagrams/ui/Anagrams.java
+++ src/com/toy/anagrams/ui/Anagrams.java
@@ -119,7 +119,6 @@ public class Anagrams extends JFrame {
         aboutMenuItem = new javax.swing.JMenuItem();
         exitMenuItem = new javax.swing.JMenuItem();
         
-
         setTitle("hasegawa_wakamatsu_ネットワーキング");
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowClosing(java.awt.event.WindowEvent evt) {
